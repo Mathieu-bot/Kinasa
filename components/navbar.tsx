d64b25f0812327649e29b7545bad1c7fe392c114
sedraValisoa <valioawalkersedra@gmail.com>
2025-05-12 14:53:11 +0300

    update : upadate ui of NavBar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export function Navbar() {
   return (
@@ -7,37 +8,7 @@ export function Navbar() {
       <div className="container flex h-16 max-w-screen-xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <svg
-              className="h-6 w-6 text-primary"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-              <path
-                d="M12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-              <path
-                d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-              <path
-                d="M12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-              />
-            </svg>
+            <Image src={"/logo.jpg"} width={50} height={50} alt="logo" />
             <span className="font-bold">Kinasa</span>
           </Link>
           <nav className="hidden md:flex items-center space-x-6 text-sm font-medium">
