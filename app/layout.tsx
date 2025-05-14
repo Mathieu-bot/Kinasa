794efc08e1fb284c720f9b86107f8a1f297f93c1
Mathieu <hei.tafita.2@gmail.com>
2025-05-15 00:07:36 +0300

    Refactor layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/components/layout/theme-provider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { AnimatedGridPattern } from "@/components/ui/animated-grid-pattern";
+import AuthProvider from "@/components/auth-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,17 +29,19 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div className="w-full relative z-0 overflow-x-hidden">
-            <div className="z-2 pt-14">{children}</div>
-            <AnimatedGridPattern className="w-full blur-sm h-full -z-10"></AnimatedGridPattern>
-          </div>
-        </ThemeProvider>
+        <AuthProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="w-full relative z-0 overflow-x-hidden">
+              <div className="z-2 pt-14">{children}</div>
+              <AnimatedGridPattern className="w-full blur-xs h-full -z-10"></AnimatedGridPattern>
+            </div>
+          </ThemeProvider>
+        </AuthProvider>
       </body>
     </html>
   );
